47bbb3f659055628219ecc53fe484591641b6227
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 16:40:12 -0300

    update chessBoardCellColor.js

--- src/chessBoardCellColor.js
+++ src/chessBoardCellColor.js
@@ -10,3 +10,29 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test chessBoardCellColor
+
+// alternative solution
+// let color = (s) => (Buffer(s)[0] - s[1]) % 2;
+// let solution = (a, b) => color(a) == color(b);
+
+// alternative solution
+// function solution(cell1, cell2) {
+//     let board = {
+//         A: 1,
+//         B: 2,
+//         C: 3,
+//         D: 4,
+//         E: 5,
+//         F: 6,
+//         G: 7,
+//         H: 8,
+//     };
+//     const total1 = board[cell1[0]] + parseInt(cell1[1]);
+//     const total2 = board[cell2[0]] + parseInt(cell2[1]);
+//     return total1 % 2 === total2 % 2;
+// }
+
+// alternative solution
+// function solution(cell1, cell2) {
+//     return (cell1.charCodeAt(0) + Number(cell1[1]) + cell2.charCodeAt(0) + Number(cell2[1])) % 2 == 0;
+// }
